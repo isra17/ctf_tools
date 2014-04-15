bc668126194fc096a2b7ebecd251393497014460
Israel Halle <isra017@gmail.com>
2014-04-15 16:18:52 -0400

    Added more utilities

---
+++ net_exec/net_exec.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+import socket
+import os, sys
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Start a process on incoming connection.')
+parser.add_argument('-p', '--port', default=31337, type=int)
+parser.add_argument('program', help='Program executed on connection')
+parser.add_argument('args', nargs='*', help='Arguments passed to the program')
+args = parser.parse_args()
+
+HOST = 'localhost'
+PORT = args.port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(1)
+
+while True:
+    conn, addr = s.accept()
+
+    print('New incomming connection...')
+
+    if os.fork() == 0:
+        print('Start %s... with params %s' % (args.program, str(args.args)) )
+        ret = subprocess.call([args.program] + args.args, stdout=conn.fileno(), stdin=conn.fileno())
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
+        print('Program terminated with status %d...' % ret)
+        sys.exit(0)
