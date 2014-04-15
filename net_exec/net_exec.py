1e39b93547bd12c27f57f0a16a8ada163c82ab90
Israel Halle <isra017@gmail.com>
2014-04-15 16:53:45 -0400

    Added setuid options

--- net_exec/net_exec.py
+++ net_exec/net_exec.py
@@ -4,9 +4,11 @@ import socket
 import os, sys
 import subprocess
 import argparse
+import pwd
 
 parser = argparse.ArgumentParser(description='Start a process on incoming connection.')
 parser.add_argument('-p', '--port', default=31337, type=int)
+parser.add_argument('-u', '--user', nargs='?')
 parser.add_argument('program', help='Program executed on connection')
 parser.add_argument('args', nargs='*', help='Arguments passed to the program')
 args = parser.parse_args()
@@ -25,6 +27,10 @@ while True:
     print('New incomming connection...')
 
     if os.fork() == 0:
+        if args.user:
+            uid = pwd.getpwnam(args.user)[2]
+            os.setuid(uid)
+
         print('Start %s... with params %s' % (args.program, str(args.args)) )
         ret = subprocess.call([args.program] + args.args, stdout=conn.fileno(), stdin=conn.fileno())
         conn.shutdown(socket.SHUT_RDWR)
