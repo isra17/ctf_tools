b1a0c757900d6e082c1c5e93ec80fc2a644b7091
Israel Halle <isra017@gmail.com>
2014-04-15 00:46:40 -0400

    Added script

---
+++ shellcode_compiler.py
@@ -0,0 +1,20 @@
+from elftools.elf.elffile import ELFFile
+import sys, os
+
+def compile(src):
+    return os.system('nasm -f elf %s' % src)
+
+def extract_shellcode(filename):
+    f = open(filename, 'rb')
+    elffile = ELFFile(f)
+
+    return elffile.get_section_by_name(b'.text').data()
+
+def get_shellcode(src):
+    filename, _ = os.path.splitext(src)
+    compile(src)
+    return extract_shellcode(filename + '.o')
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    print(repr(get_shellcode(path))[2:-1])
